8bb19795faae6b0fcf668f80d09e0668c32c7a8c
SDK Builder <sdk-builder@alipay.com>
2026-09-18 16:21:17 +0800

    Generated SDK for BCCR.

--- client/client.go
+++ client/client.go
@@ -18014,7 +18014,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.21.10"),
+				"sdk_version":      tea.String("1.22.0"),
 				"_prod_code":       tea.String("BCCR"),
 				"_prod_channel":    tea.String("undefined"),
 			}
